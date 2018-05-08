3d42e604756dbc0b965da114295f392af402e266
RESL <moritz@resl.xyz>
2018-05-08 22:44:25 +0200

    Added JM colors

--- app/main.js
+++ app/main.js
@@ -14,8 +14,8 @@ let renderer, scene, camera;
 let controls; // eslint-disable-line no-unused-vars
 let texture;
 
-(function main() {  
-  
+(function main() {
+
   setup(); // set up scene
   loop(); // start game loop
 
@@ -30,7 +30,7 @@ let texture;
 //   flip: flip horizontally?
 function equitri(offset=[0,0], r=1, flip=false) {
   let f = flip ? -1 : 1;
-  return [ 
+  return [
     offset[0] + f*2*r, offset[1] + 0,           0,
     offset[0] -   f*r, offset[1] + f*3*r/sqrt3, 0,
     offset[0] -   f*r, offset[1] - f*3*r/sqrt3, 0,
@@ -45,28 +45,28 @@ function meshgeo(r=0.01) {
   // let dy = 3 * r / sqrt3; // = a/2
   let w = 3 * r;
   let h = 3 * r / sqrt3; // = a/2
-  
+
   let nx = Math.ceil( (2+r) / w );
   let ny = Math.ceil( 2 / h );
   let pos = [];
   let uv = [];
-  
+
   // console.log(nx, ny);
-  
+
   for (let j=0; j<ny; j++) {
     for (let i=0; i<nx; i++) {
       let flip = (i+j) % 2;
       let ox = w * i + flip * r - 1; // flipped tris are offset by r to the right
       let oy = h * j - 1;
-      
+
       let tri = equitri([ox,oy], r, flip);
       pos = pos.concat( tri );
-      
+
       let c = [1-(ox+1)/2, (oy+1)/2]; // uv coordinates based on triangle center
       uv = uv.concat(c, c, c); // add 3x (for each triangle vertex)
     }
   }
-  
+
   let geo = new THREE.BufferGeometry();
   geo.addAttribute( 'position', new THREE.BufferAttribute(new Float32Array(pos), 3) );
   geo.addAttribute( 'uv', new THREE.BufferAttribute(new Float32Array(uv), 2) );
@@ -78,7 +78,7 @@ function startWebcam(append = false) {
   let videoElement = document.createElement('video');
   videoElement.autoplay = true;
   if (append) { document.body.appendChild(videoElement); }
-  
+
   navigator.mediaDevices.getUserMedia(caps).then(stream => {
     // console.log(stream);
     videoElement.srcObject = stream;
@@ -87,13 +87,13 @@ function startWebcam(append = false) {
   }).catch(err => {
     console.error('Error obtaining Webcam:', err);
   });
-  
+
   return videoElement;
 }
 
 
 function setup() {
-  
+
   renderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true
@@ -101,57 +101,61 @@ function setup() {
   renderer.setSize( W, H );
   renderer.setPixelRatio( window.devicePixelRatio );
   document.body.appendChild( renderer.domElement );
-  
+
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, W / H, 0.01, 1000 );
   controls = new THREE.OrbitControls( camera, renderer.domElement );
   camera.position.z = 2;
-  
-  
+
+
   // let v1 = equitri( [-2,0], 1, false );
   // let v2 = equitri( [2, 0], 1, true  );
   // let v = new Float32Array( v1.concat(v2) );
   // let geo = new THREE.BufferGeometry();
   // geo.addAttribute( 'position', new THREE.BufferAttribute(v, 2) );
-  
+
   // Setup webcam texture
   let videoElement = startWebcam(true);
   texture = new THREE.VideoTexture(videoElement);
   texture.minFilter = THREE.LinearFilter;
   texture.magFilter = THREE.LinearFilter;
   texture.format = THREE.RGBFormat;
-  
+
   let geo = meshgeo(0.02);
-  let mat = new THREE.MeshBasicMaterial({ color:0x1e90ff, wireframe:false, map:texture });
+  let mat = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF, // JM colors: 0x1424fa, 0xFFFFFF, 0x251e21, 0xfc4d40
+    wireframe:false,
+    map:texture
+  });
   let mesh = new THREE.Mesh( geo, mat );
   scene.add( mesh );
-  
+
   var geometry = new THREE.PlaneBufferGeometry( 2, 2*9/16, 1 );
   var material = new THREE.MeshBasicMaterial({ map:texture });
   var plane = new THREE.Mesh( geometry, material );
   plane.position.z = -0.1;
   scene.add( plane );
-  
-  
-  
+
+
+
 }
 
 
 function loop(time) { // eslint-disable-line no-unused-vars
-  
+
   requestAnimationFrame( loop );
   renderer.render( scene, camera );
-  
+
 }
 
 
 document.addEventListener('keydown', e => {
   // console.log(e.key, e.keyCode, e);
-  
+
   if (e.key == 'f') { // f .. fullscreen
     if (!document.webkitFullscreenElement) {
       document.querySelector('body').webkitRequestFullscreen();
     } else { document.webkitExitFullscreen(); }
   }
-  
+
 });
